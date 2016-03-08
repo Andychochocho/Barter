fe945e783a12e3645e77464b4f6a2204e5147f5e
alex larson <alex.m.larson@gmail.com>
2016-03-07 16:32:20 -0800

    close to figuring out GetPosts for User Object

---
+++ Tests/User_Test.cs
@@ -0,0 +1,43 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BarterNamespace
+{
+  public class UserTest : IDisposable
+  {
+    public UserTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=barter_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_GetPosts()
+    {
+
+      User testUser = new User("steve@aol.com","pic","Password","location");
+      testUser.Save();
+
+      UserPost testPost = new UserPost(5, "Magic Johnson", new DateTime(2000, 1, 1));
+      testPost.Save();
+      //Act
+
+      List<UserPost> result = testUser.GetPosts();
+      Console.WriteLine(testUser.GetEmail());
+      Console.WriteLine(testPost.GetUserPost());
+      Console.WriteLine(result.Count);
+      List<UserPost> testList = new List<UserPost> {testPost};
+      //Assert
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Dispose()
+    {
+      User.DeleteAll();
+      UserPost.DeleteAll();
+    }
+  }
+}
