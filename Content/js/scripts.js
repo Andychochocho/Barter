cf9d77cbac2940db08b7d229602fef23f7a0aa8f
simon <simonctemple@live.com>
2016-03-10 16:37:35 -0800

    fix model not showing

--- Content/js/scripts.js
+++ Content/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(".btn-message").click(function(event) {
-    $("#message-modal").fadeToggle();
+    $(".message-modal").fadeToggle();
     $(".modal-transparency").fadeToggle();
   });
   $(".modal-close").click(function(event) {
